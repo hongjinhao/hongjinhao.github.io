0b0856e733c68ed11087f4773f03b623c793d25f
hongjinhao <50015504+hongjinhao@users.noreply.github.com>
2024-11-09 21:08:43 -0800

    Fixing a bug in the liked button shape
    There is a bug where the liked button looks the same for both states on an android mobile phone.

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const likeBtn = document.getElementById("likeButton");
 const likeCount = document.getElementById("likeCount");
 
 likeBtn.onclick = function() {
-    if (likeBtn.classList.contains("liked")) {
-        likeBtn.classList.remove("liked");
+    if (likeBtn.classList.getAttribute("data-liked")) {
+        likeBtn.innerHTML = "&#9825;"; // Outlined heart
+        likeBtn.setAttribute("data-liked", "false");
         NumOfLikes--;
         likeCount.textContent = NumOfLikes;
     } else {
-        likeBtn.classList.add("liked");
+        likeButton.innerHTML = "&#9829;"; // Filled heart
+        likeBtn.setAttribute("data-liked", "true");
         NumOfLikes++;
         likeCount.textContent = NumOfLikes;
     }
