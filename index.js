f3e15221194f5c9d3a08ddce174396f6ac9d7bf7
hongjinhao <50015504+hongjinhao@users.noreply.github.com>
2024-11-09 21:14:21 -0800

    Fixing the liked button bug again
    Trying to use different shapes this time

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ const likeBtn = document.getElementById("likeButton");
 const likeCount = document.getElementById("likeCount");
 
 likeBtn.onclick = function() {
-    if (likeBtn.classList.getAttribute("data-liked")) {
+    if (likeBtn.getAttribute("data-liked") == "true") {
         likeBtn.innerHTML = "&#9825;"; // Outlined heart
         likeBtn.setAttribute("data-liked", "false");
+        likeBtn.classList.remove("liked")
         NumOfLikes--;
         likeCount.textContent = NumOfLikes;
     } else {
         likeButton.innerHTML = "&#9829;"; // Filled heart
         likeBtn.setAttribute("data-liked", "true");
+        likeBtn.classList.add("liked")
         NumOfLikes++;
         likeCount.textContent = NumOfLikes;
     }
