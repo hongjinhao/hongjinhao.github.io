2fea9858412acd55ffe9c31c3fcd6561d081ff22
hongjinhao <50015504+hongjinhao@users.noreply.github.com>
2024-12-11 15:29:21 -0800

    getlikeCount function
    Setup getLikeCount async function to access the like counts from the backend API

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ let NumOfLikes = 0;
 const likeBtn = document.getElementById("likeButton");
 const likeCount = document.getElementById("likeCount");
 
+/*
+    Interactive Like Button: Heart Shape Symbol
+    When pressed, alternate between the two states: on and off. 
+    Simultaneously, likeCount += 1
+    likeBtn has an onclick HTML event that takes in a function
+    Our defined function expression changes HTML, set attributes and change classLists
+*/ 
 likeBtn.onclick = function() {
     if (likeBtn.getAttribute("data-liked") == "true") {
         likeBtn.innerHTML = "&#9825;"; // Outlined heart
@@ -18,3 +25,18 @@ likeBtn.onclick = function() {
     }
 
 }
+
+// Asynchronous function to retrieve number of likes from Backend 
+async function getLikeCount() {
+  try {
+    const response = await fetch('http://localhost:8080/api/likeCount'); // Wait for the fetch to complete
+    const data = await response.json(); // Wait for the JSON to be parsed
+    console.log(data.likeCount); // Access and log the likeCount property
+    likeCount.innerHTML = data.likeCount;
+    NumOfLikes = data.likeCount;
+  } catch (error) {
+    console.error('Error fetching likeCount:', error); // Handle errors
+  }
+}
+
+getLikeCount();
--- readme.md
+++ readme.md
@@ -2,4 +2,6 @@
 
 This repo contains all the files for the github.io site which hosts my resume. 
 
-The idea is to use a NFC card and direct people to this website as a fun demonstration of the internet. 
+The idea is to use a NFC card and direct people to this website as a fun demonstration of JS, HTML and CSS. 
+
+This website is deployed through GitHub Pages. To view the live version, visit [hongjinhao.github.io](https://hongjinhao.github.io).
